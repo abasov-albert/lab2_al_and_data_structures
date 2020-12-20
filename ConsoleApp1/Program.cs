7f435a459f6f044c6daeea6cf0f2718f40d142e2
abasov-albert <abasovalik2001@gmail.com>
2020-12-21 00:44:59 +0300

    Refactoring

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-    using System.Collections;
-    using System.Collections.Generic;
 using ConsoleApp1.Model;
 
 namespace ConsoleApp1
@@ -9,10 +7,19 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            /*
-            MyList<string> list = new MyList<string>();
+            // TestLinkedList();
             
-            string text = System.IO.File.ReadAllText(@"C:\Users\abaso\RiderProjects\lab2_al_and_data_structures\ConsoleApp1\Text.txt");
+            // TestHashTable();
+            
+            // TestTree();
+        }
+        
+        private static void TestLinkedList()
+        {
+            MyList<string> list = new MyList<string>();
+
+            string text =
+                System.IO.File.ReadAllText(@"C:\Users\abaso\RiderProjects\lab2_al_and_data_structures\ConsoleApp1\Text.txt");
             text = text.Replace("\n", " ");
             var words = text.Split(" ");
 
@@ -23,9 +30,9 @@ namespace ConsoleApp1
 
             Console.WriteLine("Before the deletion of word \"or\"");
             Console.WriteLine(list.ToString());
-            
+
             Console.WriteLine();
-            
+
             list.Remove("or");
             Console.WriteLine("After the deletion of word \"or\"");
             Console.WriteLine(list.ToString());
@@ -39,15 +46,16 @@ namespace ConsoleApp1
             Console.WriteLine();
 
             Console.WriteLine("Formatted text alligned right");
-            list.PrintFormatted();    
-            */
-            
-            /*
+            list.PrintFormatted();
+        }
+        
+        private static void TestHashTable()
+        {
             Marks formA = new Marks(72, "well", 63);
             Marks formB = new Marks(94, "not bad", 87);
             Marks formC = new Marks(72, "well", 63);
             Marks formD = new Marks(94, "not bad", 87);
-            
+
             MyHashTable<string, Marks> marks = new MyHashTable<string, Marks>();
             marks.Add("Lena", formB);
             marks.Add("Rita", formC);
@@ -55,7 +63,7 @@ namespace ConsoleApp1
             marks.Add("Denis", formA);
             marks.Add("Abasov Albert", formA);
             marks.Add("Ivan Ivanov", formB);
-            
+
             Console.WriteLine("Ivan Ivanov: " + marks.Get("Ivan Ivanov"));
             Console.WriteLine("Lena: " + marks.Get("Lena"));
             Console.WriteLine("Rita: " + marks.Get("Rita"));
@@ -64,10 +72,12 @@ namespace ConsoleApp1
             Console.WriteLine("Kolya: " + marks.Get("Kolya"));
             marks.Remove("Denis");
             Console.WriteLine("Denis: " + marks.Get("Denis"));
-            */
-            
+        }
+        
+        private static void TestTree()
+        {
             //  tree
-            
+
             var myBinaryTree = new MyBinaryTree<int, string>();
             myBinaryTree.Add(47, "Math");
             myBinaryTree.Add(40, "Physics");
@@ -84,20 +94,19 @@ namespace ConsoleApp1
             myBinaryTree.Add(60, "Law");
             myBinaryTree.Add(71, "French");
             myBinaryTree.Add(32, "Psychology");
-            
-            
+
+
             myBinaryTree.Remove(47);
             myBinaryTree.Remove(22);
             myBinaryTree.Remove(61);
-            
+
             Console.WriteLine(myBinaryTree.Get(38));
             Console.WriteLine(myBinaryTree.Get(60));
             Console.WriteLine(myBinaryTree.Get(32));
-            
+
             Console.WriteLine();
 
             Console.WriteLine($"Maximum value is {myBinaryTree.GetMax()}");
-            
         }
     }
 }
