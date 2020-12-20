4090a326f570237e4e245341c1d352e9a1487ae6
abasov-albert <abasovalik2001@gmail.com>
2020-12-21 01:31:26 +0300

    Changed Hash Table

---
+++ ConsoleApp1/Model/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1.Model
+{
+    public class Student : IComparable<Student>
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int grade { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            int nameComparison = this.name.CompareTo(other.name);
+            if (nameComparison != 0)
+                return nameComparison;
+
+            var surnameComparison = this.surname.CompareTo(other.surname);
+            if (surnameComparison != 0)
+                return surnameComparison;
+
+            return this.grade.CompareTo(other.grade);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = 23 * hash + name == null ? 0 : name.GetHashCode();
+            hash = 23 * hash + surname == null ? 0 : surname.GetHashCode();
+            hash = 23 * hash + grade;
+
+            return hash;
+        }
+
+        public Student(string name, string surname, int grade)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.grade = grade;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
         {
             // TestLinkedList();
             
-            // TestHashTable();
+            TestHashTable();
             
             // TestTree();
         }
@@ -51,27 +51,31 @@ namespace ConsoleApp1
         
         private static void TestHashTable()
         {
-            Marks formA = new Marks(72, "well", 63);
-            Marks formB = new Marks(94, "not bad", 87);
-            Marks formC = new Marks(72, "well", 63);
-            Marks formD = new Marks(94, "not bad", 87);
+            Grades grade1 = new Grades(72, "well", 63);
+            Grades grade2 = new Grades(94, "not bad", 87);
+            Grades grade3 = new Grades(72, "well", 63);
+            Grades grade4 = new Grades(94, "not bad", 87);
+            
+            Student student1 = new Student("Vladislav", "Vladislavov", 1);
+            Student student2 = new Student("Ivan", "Ivanov", 2);
+            Student student3 = new Student("Petr", "Petrov", 3);
+            Student student4 = new Student("Sydor", "Sydorov", 4);
+            
 
-            MyHashTable<string, Marks> marks = new MyHashTable<string, Marks>();
-            marks.Add("Lena", formB);
-            marks.Add("Rita", formC);
-            marks.Add("Valera", formD);
-            marks.Add("Denis", formA);
-            marks.Add("Abasov Albert", formA);
-            marks.Add("Ivan Ivanov", formB);
+            MyHashTable<Student, Grades> studentToGrades = new MyHashTable<Student, Grades>();
+            studentToGrades.Add(student1, grade1);
+            studentToGrades.Add(student2, grade2);
+            studentToGrades.Add(student3, grade3);
+            studentToGrades.Add(student4, grade4);
 
-            Console.WriteLine("Ivan Ivanov: " + marks.Get("Ivan Ivanov"));
-            Console.WriteLine("Lena: " + marks.Get("Lena"));
-            Console.WriteLine("Rita: " + marks.Get("Rita"));
-            Console.WriteLine("Valera: " + marks.Get("Valera"));
-            Console.WriteLine("Denis: " + marks.Get("Denis"));
-            Console.WriteLine("Kolya: " + marks.Get("Kolya"));
-            marks.Remove("Denis");
-            Console.WriteLine("Denis: " + marks.Get("Denis"));
+            Console.WriteLine("Ivan Ivanov: " + studentToGrades.Get(student2));
+            Console.WriteLine("Vladislav Vladislavov: " + studentToGrades.Get(student1));
+            Console.WriteLine("Petr Petrov: " + studentToGrades.Get(student3));
+            Console.WriteLine("Sydor Sydorov: " + studentToGrades.Get(student4));
+            
+            studentToGrades.Remove(student3);
+            Console.WriteLine();
+            Console.WriteLine("Petr Petrov: " + studentToGrades.Get(student3));
         }
         
         private static void TestTree()
